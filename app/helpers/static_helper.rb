f3dd43f195f3d0220b3ffc94b8a07a048653997e
codystringham <cs.negativ@gmail.com>
2016-03-10 20:54:35 -0700

    adds basic content for indexing

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,9 @@
 module StaticHelper
+
+  def nav_link(name, path)
+    haml_tag :li, class: "#{'active' if params[:action] == name}" do
+      haml_tag :a, name.titleize, href: path
+    end
+  end
+
 end
