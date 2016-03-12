a94b586e7c712272bd55faf12408539ea38e80ce
codystringham <cs.negativ@gmail.com>
2016-03-12 08:23:07 -0700

    finishes content

--- app/assets/javascripts/static.coffee
+++ app/assets/javascripts/static.coffee
@@ -1,2 +1,7 @@
 $ ->
   $('.button-collapse').sideNav()
+
+
+$(document).on 'click', '.nav-link', ->
+  el = $(@).attr('href')
+  $('html,body').animate { scrollTop: ($(el).offset().top - 90) }, 'slow'
--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,8 +1,8 @@
 module StaticHelper
 
   def nav_link(name, path)
-    haml_tag :li, class: "#{'active' if params[:action] == name}" do
-      haml_tag :a, name.titleize, href: path
+    haml_tag :li, class: "" do
+      haml_tag :a, name.titleize, href: path, class: 'nav-link'
     end
   end
 
